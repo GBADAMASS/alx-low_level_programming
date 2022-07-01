4ead688443d7db7f5e762d3cb4223d9275edcfff
GBADAMASS <gbadamassia@gmail.com>
2022-07-01 12:47:56 -0700

    second commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Return: Always 0 (Success/correct)
  *
  *     */
 
@@ -24,26 +24,38 @@ int main(void)
 
 
 
-			srand(time(0));
+		srand(time(0));
 
-				n = rand() - RAND_MAX / 2;
-
-
-
-					if (n == 0)
-
-								printf("%d is zero\n", n);
-
-						else if (n < 0)
-
-									printf("%d is negative\n", n);
-
-							else
-
-										printf("%d is positive\n", n);
+		n = rand() - RAND_MAX / 2;
 
 
 
-								return (0);
+		if (n == 0)
+
+		{
+
+			printf("%d is zero\n", n);
+
+		}
+
+		else if (n < 0)
+
+		{
+
+			printf("%d is negative\n", n);
+
+		}
+
+		else
+
+		{
+
+			printf("%d is positive\n", n);
+
+		}
+
+
+
+		return (0);
 
 }
