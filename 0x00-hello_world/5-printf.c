a75df8452157e07df897e1052e517b58703a0b74
GBADAMASS <gbadamassia@gmail.com>
2022-07-04 13:14:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Description: print character with new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
-
 }
