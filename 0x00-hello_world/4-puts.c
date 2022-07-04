947e4bd39c8c1f116d8ccf026de374c743234f33
GBADAMASS <gbadamassia@gmail.com>
2022-07-04 13:55:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: print a character with puts
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
