4570cb27c0fc62ce27388ea89a9ccd8302f6a5c5
GBADAMASSI ABDOU-AKIM <gbadamassia@gmail.com>
2022-07-09 23:00:59 +0000

    alx low level programming GBAA

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two number
+ * @x: operand to be added
+ * @y: operand to be added
+ * Return: integer value
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
